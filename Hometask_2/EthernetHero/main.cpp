0b99b77082a21ee04c32e2db30cdd5683f4957be
Ivan Senin <senin-i-i@yandex.ru>
2013-09-11 15:57:22 +0400

    (Done)  removing code for debug

--- Hometask_2/EthernetHero/main.cpp
+++ Hometask_2/EthernetHero/main.cpp
@@ -1,23 +1,10 @@
-#include <QApplication>
+#include <QtWidgets/QApplication>
 
 #include "mainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
-
-
-    if (argc > 1)
-    {
-        int i = 0;
-        for (int j = 0; j < 999999999; j++)
-        {
-            i += (j % 2);
-        }
-        if (i < 100)
-            i = 1;
-        return 1;
-    }
 
     MainWindow gameWindow;
     gameWindow.show();
